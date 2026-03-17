05b20e623533918d3c491783171661fce4073187
RyuMitsuky <ryu.mitsuky.87@gmail.com>
2026-03-16 23:59:56 -0500

    feat :zap: add changes in tomcat

--- src/main/java/com/example/LogiTrack/LogiTrackApplication.java
+++ src/main/java/com/example/LogiTrack/LogiTrackApplication.java
@@ -2,12 +2,18 @@ package com.example.LogiTrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LogiTrackApplication {
+public class LogiTrackApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LogiTrackApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogiTrackApplication.class, args);
 	}
-
 }
